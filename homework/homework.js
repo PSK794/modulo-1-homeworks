17e6a859fae123c4eef339ec961cf5390c6c9ccd
TomasTinto1234 <tititinto@gmail.com>
2022-03-28 16:26:01 -0300

    primer homework

---
+++ homework/homework.js
@@ -0,0 +1,59 @@
+'use strict'
+
+function BinarioADecimal(num) {
+  // tu codigo aca
+  
+  let sume = num.split("").reverse()
+
+  var binario = 0  
+  
+
+  for( var i = 0 ; i < sume.length; i++) {
+   
+    binario += 2** i * sume[i] 
+  }
+  return binario
+}
+
+
+function DecimalABinario(num) {
+  // tu codigo aca
+
+  
+
+  var decimal = "" 
+
+    while(num > 0) {
+
+     decimal = (num % 2) + decimal
+
+    num = Math.floor(num / 2) 
+
+}
+  return decimal
+
+}
+module.exports = {
+  BinarioADecimal,
+  DecimalABinario,
+}
+
+
+
+
+
+
+/*
+
+
+var restul = ""
+
+while(num > 0) {
+  restul = (num % 2) + restul
+
+  num = math.floor(num/2)
+
+}
+return restul
+
+*/
